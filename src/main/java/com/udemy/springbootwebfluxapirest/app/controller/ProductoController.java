85d930217445886e231e69ecd6c37d1b33fb6f72
mfernandez <mfernandez@soaint.com>
2023-06-15 20:17:37 -0400

    V66. manejo de errores en el crear

--- src/main/java/com/udemy/springbootwebfluxapirest/app/controller/ProductoController.java
+++ src/main/java/com/udemy/springbootwebfluxapirest/app/controller/ProductoController.java
@@ -2,6 +2,7 @@ package com.udemy.springbootwebfluxapirest.app.controller;
 
 import com.udemy.springbootwebfluxapirest.app.model.Producto;
 import com.udemy.springbootwebfluxapirest.app.service.ProductoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
@@ -10,12 +11,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -29,28 +33,29 @@ public class ProductoController {
     private String path;
 
     @PostMapping("/confoto")
-    public Mono<ResponseEntity<Producto>> crearConFoto(Producto producto, @RequestPart FilePart file){
+    public Mono<ResponseEntity<Producto>> crearConFoto(@Valid Mono<Producto> monoProducto, @RequestPart FilePart file) {
 
-        if(producto.getCreateAt()==null) {
-            producto.setCreateAt(new Date());
-        }
+        return monoProducto.flatMap(producto -> {
+            if (producto.getCreateAt() == null) {
+                producto.setCreateAt(new Date());
+            }
 
-        producto.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
-                .replace(" ", "")
-                .replace(":", "")
-                .replace("\\", ""));
+            producto.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+                    .replace(" ", "")
+                    .replace(":", "")
+                    .replace("\\", ""));
 
-        return file.transferTo(new File(path + producto.getFoto())).then(productoService.save(producto))
-                .map(p-> ResponseEntity
-                        .created(URI.create("/api/productos/".concat(p.getId())))
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .body(p)
-                );
-
+            return file.transferTo(new File(path + producto.getFoto())).then(productoService.save(producto))
+                    .map(p -> ResponseEntity
+                            .created(URI.create("/api/productos/".concat(p.getId())))
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .body(p)
+                    );
+        });
     }
 
     @PostMapping("/upload/{id}")
-    public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPart FilePart file){
+    public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPart FilePart file) {
         return productoService.findById(id).flatMap(p -> {
                     p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
                             .replace(" ", "")
@@ -74,33 +79,61 @@ public class ProductoController {
     }
 
     @PostMapping
-    public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto) {
-        if (producto.getCreateAt() == null) {
-            producto.setCreateAt(new Date());
-        }
-        return productoService.save(producto).map(p -> ResponseEntity
-                .created(URI.create("api/producto/".concat(p.getId())))
-                .body(p));
+    public Mono<ResponseEntity<Map<String, Object>>> crear(@Valid @RequestBody Mono<Producto> monoProducto){
+
+        Map<String, Object> respuesta = new HashMap<String, Object>();
+
+        return monoProducto.flatMap(producto -> {
+            if(producto.getCreateAt()==null) {
+                producto.setCreateAt(new Date());
+            }
+
+            return productoService.save(producto).map(p-> {
+                respuesta.put("producto", p);
+                respuesta.put("mensaje", "Producto creado con éxito");
+                respuesta.put("timestamp", new Date());
+                return ResponseEntity
+                        .created(URI.create("/api/productos/".concat(p.getId())))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .body(respuesta);
+            });
+
+        }).onErrorResume(t -> {
+            return Mono.just(t).cast(WebExchangeBindException.class)
+                    .flatMap(e -> Mono.just(e.getFieldErrors()))
+                    .flatMapMany(Flux::fromIterable)
+                    .map(fieldError -> "El campo "+fieldError.getField() + " " + fieldError.getDefaultMessage())
+                    .collectList()
+                    .flatMap(list -> {
+                        respuesta.put("errors", list);
+                        respuesta.put("timestamp", new Date());
+                        respuesta.put("status", HttpStatus.BAD_REQUEST.value());
+                        return Mono.just(ResponseEntity.badRequest().body(respuesta));
+                    });
+
+        });
+
+
     }
 
     @DeleteMapping("/{id}")
     public Mono<ResponseEntity<Void>> eliminar(@PathVariable String id) {
-        return productoService.findById(id).flatMap(p-> {
+        return productoService.findById(id).flatMap(p -> {
             return productoService.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
-        }).defaultIfEmpty( new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+        }).defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("/{id}")
     public Mono<ResponseEntity<Producto>> editar(@PathVariable String id, @RequestBody Producto producto) {
 
         return productoService.findById(id).flatMap(p -> {
-            p.setNombre(producto.getNombre());
-            p.setPrecio(producto.getPrecio());
-            p.setCategoria(producto.getCategoria());
-            return productoService.save(p);
-        }).map(p -> ResponseEntity
-                .created(URI.create("api/producto/".concat(p.getId())))
-                .body(p))
+                    p.setNombre(producto.getNombre());
+                    p.setPrecio(producto.getPrecio());
+                    p.setCategoria(producto.getCategoria());
+                    return productoService.save(p);
+                }).map(p -> ResponseEntity
+                        .created(URI.create("api/producto/".concat(p.getId())))
+                        .body(p))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
